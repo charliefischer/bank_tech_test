1086d8f92c35b6a34b9b786c82bd6dc39c141451
Charlie Fischer <charlie.a.fischer@gmail.com>
2021-03-12 11:23:12 +0000

    Date mock in transaction.unit.test

--- src/transaction.js
+++ src/transaction.js
@@ -1,8 +1,8 @@
 const FindDate = require('./findDate')
 
 class Transaction {
-  constructor(type, amount, balance) {
-    this.date = this.#findDate()
+  constructor(type, amount, balance, date = this.#findDate()) {
+    this.date = date
     this.type = type
     this.amount = amount
     this.balance = balance
--- tests/transaction.unit.test.js
+++ tests/transaction.unit.test.js
@@ -1,11 +1,20 @@
 const Transaction = require('../src/transaction')
+
 const simplifiedDate = require('./helpers')
 let date = simplifiedDate()
+const mockDate = jest.fn(() => '1/1/2000')
+
 
 test('the deposit amount,date and balance are passed into transactions', () => {
-  let transaction = new Transaction('credit', 10, 10.00, date)
+  let transaction = new Transaction('credit', 10, 10.00)
   expect(transaction.type).toEqual('credit')
   expect(transaction.amount).toEqual(10)
   expect(transaction.balance).toEqual(10.00)
   expect(transaction.date).toEqual(date)
+})
+
+test('the transactions can take a hardcoded date', () => {
+  let transaction = new Transaction('credit', 10, 10.00, mockDate())
+
+  expect(transaction.date).toEqual('1/1/2000')
 })
